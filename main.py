b31f44a2051cc6598ddd625d9e97e3bda7c7ce74
Krishnav <79277882+Krishnav-Rajbangshi@users.noreply.github.com>
2021-10-02 20:31:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ def input_image(path, size):
 
 def main():
     img_size = (224, 224)
-    train_path = ''
+    train_path = r'C:\Users\Asus\Desktop\ML (self work)\Deep Learning\Projects\Road\Datasets'
 
     model = Classifier(img_size, train_path)
 
     model.train()
 
-    img_path = ''
+    img_path = r'C:\Users\Asus\Desktop\ML (self work)\Deep Learning\Projects\Road\Evaluate\6.jpg'
     image = input_image(img_path, img_size)
 
     prediction = model.predict_image(image)
